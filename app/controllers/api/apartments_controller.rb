e663b2fc095d032e429a0b597dc285b343de0c89
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-15 12:21:49 -0500

    adding create action

--- app/controllers/api/apartments_controller.rb
+++ app/controllers/api/apartments_controller.rb
@@ -8,4 +8,14 @@ class Api::ApartmentsController < ApplicationController
     @apartment = Apartment.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @apartment = Apartment.new(
+      neighborhood: params[:neighborhood],
+      building_type: params[:building_type],
+      rent: params[:rent]
+    )
+    @apartment.save
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/apartments" => "apartments#index"
     get "/apartments/:id" => "apartments#show"
+    post "/apartments" => "apartments#create"
   end
 end
